4db48ccd78d82bc1d5dc4765694cff1c48ae508c
Robert <wi21b059@technikum-wien.at>
2022-09-05 18:20:26 +0200

    initial commit

---
+++ src/test/java/at/fhtechnikum/areas/AreasApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.areas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
