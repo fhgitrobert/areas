5e20e38f78a9c43f12c618ed8a4a33caad415bd9
Robert <wi21b059@technikum-wien.at>
2022-09-05 18:43:38 +0200

    implementing area calculation

--- src/main/java/at/fhtechnikum/areas/areas.java
+++ src/main/java/at/fhtechnikum/areas/areas.java
@@ -6,4 +6,15 @@ public class areas {
         return "Kreis - Radius \n Quadrat - Seite \n Rechteck - Länge/Breite";
     }
 
+    public double getArea(String form, double value){
+        if(form.equals("Kreis")){
+            return value*value*Math.PI;
+        }
+        else return value*value;
+    }
+
+    public double getArea(String form, double val1, double val2){
+        return val1*val2;
+    }
+
 }
